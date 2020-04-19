0ff288aa431082a284395ec9676d47c1201d4d04
Anwesh43 <anweshthecool0@gmail.com>
2020-04-20 04:51:51 +0530

    adding FireBallBack class

--- fireballbackuiview/src/main/java/com/anwesh/uiprojects/fireballbackuiview/FireBallBackView.kt
+++ fireballbackuiview/src/main/java/com/anwesh/uiprojects/fireballbackuiview/FireBallBackView.kt
@@ -160,4 +160,28 @@ class FireBallBackView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class FireBallBack(var i : Int, val state : State = State()) {
+
+        private val root : FBBNode = FBBNode(0)
+        private var curr : FBBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
