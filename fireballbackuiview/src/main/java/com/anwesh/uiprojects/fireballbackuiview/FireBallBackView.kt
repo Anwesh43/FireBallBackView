f4ef27dbac99e9b3a8c0602280d4c796c366e927
Anwesh43 <anweshthecool0@gmail.com>
2020-04-20 04:43:29 +0530

    adding FireBallBackView class

--- fireballbackuiview/src/main/java/com/anwesh/uiprojects/fireballbackuiview/FireBallBackView.kt
+++ fireballbackuiview/src/main/java/com/anwesh/uiprojects/fireballbackuiview/FireBallBackView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawFBBNode(i : Int, scale : Float, paint : Paint) {
     drawFireBallBack(scale, size, w, paint)
     restore()
 }
+
+class FireBallBackView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
