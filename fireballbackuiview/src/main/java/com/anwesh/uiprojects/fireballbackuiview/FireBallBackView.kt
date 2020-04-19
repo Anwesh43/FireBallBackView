69ddd413bec9db598561cea4bbe8d4e6f294d8fc
Anwesh43 <anweshthecool0@gmail.com>
2020-04-20 04:32:18 +0530

    adding FireBallBackView class

---
+++ fireballbackuiview/src/main/java/com/anwesh/uiprojects/fireballbackuiview/FireBallBackView.kt
@@ -0,0 +1,24 @@
+package com.anwesh.uiprojects.fireballbackuiview
+
+/**
+ * Created by anweshmishra on 20/04/20.
+ */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val lines : Int = 4
+val strokeFactor : Float = 2.9f
+val sizeFactor : Float = 0.02f
+val scGap : Float = 0.02f
+val squareColor : Int = Color.parseColor("#3F51B5")
+val ballColor : Int = Color.parseColor("#673AB7")
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
